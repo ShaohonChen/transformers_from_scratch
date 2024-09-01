672b397b3db56e00264ba4e28dee2002ae19d994
ShaohonChen <shaohon_chen@115lab.club>
2024-09-01 15:16:46 +0800

    增加一些注释

--- pretrain.py
+++ pretrain.py
@@ -11,7 +11,7 @@ def main():
 
     # load dataset
     raw_datasets = datasets.load_dataset(
-        "json", data_files="data/wikipedia-cn-20230720-filtered.json"
+        "json", data_files="data/wikipedia-zh-cn-20240820.json"
     )
 
     raw_datasets = raw_datasets["train"].train_test_split(test_size=0.1, seed=2333)
@@ -59,28 +59,31 @@ def main():
     )
     model = transformers.Qwen2ForCausalLM(config)
     model_size = sum(t.numel() for t in model.parameters())
-    print("Model setting")
+    print("Model Config:")
     print(config)
-    print(f"Model size: {model_size/1000**2:.1f}M parameters")
+    print(f"Model Size: {model_size/1000**2:.1f}M parameters")
 
     # train
     args = transformers.TrainingArguments(
         output_dir="checkpoints",
-        per_device_train_batch_size=24,
-        per_device_eval_batch_size=24,
+        per_device_train_batch_size=24,  # 每个GPU的训练batch数
+        per_device_eval_batch_size=24,  # 每个GPU的测试batch数
         eval_strategy="steps",
-        eval_steps=2_000,
+        eval_steps=5_000,
         logging_steps=500,
-        gradient_accumulation_steps=12,
+        gradient_accumulation_steps=12,  # 梯度累计总数
         num_train_epochs=1,
         weight_decay=0.1,
         warmup_steps=1_000,
-        lr_scheduler_type="cosine",
-        learning_rate=5e-4,
-        save_steps=2_000,
+        optim="adamw_torch",  # 优化器使用adamw
+        lr_scheduler_type="cosine",  # 学习率衰减策略
+        learning_rate=5e-4,  # 基础学习率，
+        save_steps=5_000,
         save_total_limit=10,
-        fp16=True,
+        bf16=True,  # 开启bf16训练, 对于Amper架构以下的显卡建议替换为fp16=True
     )
+    print("Train Args:")
+    print(args)
     # enjoy training
     trainer = transformers.Trainer(
         model=model,
@@ -94,11 +97,19 @@ def main():
     trainer.train()
 
     # save model
-    model.save_pretrained("./output/")
+    model.save_pretrained("./output/")  # 保存模型的路径
 
     # generate
     pipe = transformers.pipeline("text-generation", model=model, tokenizer=tokenizer)
-    print(pipe("陕西省是", num_return_sequences=1)[0]["generated_text"])
+    print("GENERATE:", pipe("人工智能", num_return_sequences=1)[0]["generated_text"])
+    prompts = ["牛顿是", "北京市的人口", "太平洋的面积"]
+    examples = []
+    for i in range(3):
+        # 根据提示词生成数据
+        text = pipe(prompts[i], num_return_sequences=1)[0]["generated_text"]
+        text = swanlab.Text(text)
+        examples.append(text)
+    swanlab.log({"Generate": examples})
 
 
 if __name__ == "__main__":
